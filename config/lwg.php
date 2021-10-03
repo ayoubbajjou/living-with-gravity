c27aa6f3d4732b6cb6f5aae32006c82bb7330708
Ayoub bajjou <bajjouayoub@gmail.com>
2021-10-03 18:21:59 +0100

    get posts from wordpress website as wp_api

---
+++ config/lwg.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Wordpress API
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'wp_api' => env('WP_API', 'https://livingwithgravity.com/wp-json/wp/v2/'),
+    'wp_api_posts' => env('WP_API_POSTS', 'https://livingwithgravity.com/wp-json/wp/v2/posts'),
+
+];
